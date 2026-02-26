fe3755c825a26bc31759bf6a3bb195aaf1493a02
David Almeida <davidalmeida.gamedev@gmail.com>
2026-02-26 17:47:15 +0000

    Trabalho Finalizado

--- TempConvert/Program.cs
+++ TempConvert/Program.cs
@@ -1,6 +1,19 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Specialized;
+using System.ComponentModel;
 using System.Data;
 using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Globalization;
+using System.Runtime.CompilerServices;
+using System.Reflection.Metadata;
+using System.Data.SqlTypes;
+using System.Data.Common;
+using System.Reflection.PortableExecutable;
+using System.Reflection;
+using System.Reflection.Emit;
 
 namespace TempConvert
 {
@@ -8,10 +21,41 @@ namespace TempConvert
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("What is the Temperature?"); //Prints "What is the Temperature?"
-            Console.ReadLine(); //Asks for the user's input
-            Console.WriteLine("Celsius (C) or Fahrenheit (F)?"); //Prints "Celsius (C) or Fahrenheit (F)?"
-            Console.ReadLine(); //Asks for the user's input
+            CultureInfo.CurrentCulture = CultureInfo.InvariantCulture;
+
+            Console.Write("Temperature: ");
+            string Temperature1 = Console.ReadLine();
+            double Temperature2 = Convert.ToDouble(Temperature1);
+
+            Console.Write("Unit (C/F): ");
+            string Mode = Console.ReadLine();
+
+            double Calculate1 = Temperature2;
+            double Calculate2 = Temperature2;
+
+            double Result1 = Calculate1 * 1.8 + 32;
+            double Absolute1 = Math.Abs(Result1);
+
+            double Result2 = (Calculate2 - 32) / 1.8;
+            double Absolute2 = Math.Abs(Result2);
+
+
+            switch (Mode)
+            {
+                case "C":
+                    Console.WriteLine($"{Calculate1:f2} C = {Result1:f2} F");
+                    Console.WriteLine($"Absolute value: {Absolute1:f2}");
+                    break;
+
+                case "F":
+                    Console.WriteLine($"{Calculate2:f2} F = {Result2:f2} C");
+                    Console.WriteLine($"Absolute value: {Absolute2:f2}");
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid unit.");
+                    break;
+            }
         }
     }
 }
